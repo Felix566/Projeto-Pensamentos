bc30473f2b7b37401cf3d3bf8a0a870b6ccc7f23
Leandro Felix <leandro123felix123@gmail.com>
2023-04-24 16:18:50 -0300

    Criando os models

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 const flash = require("express-flash");
@@ -8,8 +8,12 @@ const app = express();
 
 const conn = require("./db/conn");
 
+// Models
+const Tought = require("./models/Tought");
+const User = require("./models/User");
+
 // template engine
-app.engine("handlebars", exphbs());
+app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
 // receber resposta do body
